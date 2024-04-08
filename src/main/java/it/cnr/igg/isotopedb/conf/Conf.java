516a4a74def41dc6920693da2111e0085f09b3b9
ericperrone <87177481+ericperrone@users.noreply.github.com>
2024-04-08 11:28:23 +0200

    Update Conf.java

--- src/main/java/it/cnr/igg/isotopedb/conf/Conf.java
+++ src/main/java/it/cnr/igg/isotopedb/conf/Conf.java
@@ -13,8 +13,19 @@ public class Conf {
 			prop.load(input);
 			return prop;
 		} catch (IOException ex) {
-			ex.printStackTrace();
-			throw ex;
+//			ex.printStackTrace();
+//			throw ex;
+			return loadDefault();
 		}
+	}
+	
+	private static Properties loadDefault() {
+		Properties prop = new Properties();
+		prop.setProperty("driver", "org.postgresql.Driver");
+		prop.setProperty("url", "jdbc:postgresql://192.168.0.5:5432/itineris_wp8");
+		prop.setProperty("user", "itineris_wp8_user");
+		prop.setProperty("password", "9a1451ce4b899fac7f5f");
+		prop.setProperty("db", "itineris_wp8");
+		return prop;
 	}
 }
