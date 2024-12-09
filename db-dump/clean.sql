4a481183d028be8401b4c498f5e5582ccc329eb8
ericperrone <87177481+ericperrone@users.noreply.github.com>
2024-12-09 14:30:16 +0100

    aggiornamento dump

---
+++ db-dump/clean.sql
@@ -0,0 +1,7 @@
+delete from sample_matrix ;
+delete from dataset_authors ;
+delete from authors;
+delete from sample_attribute ;
+delete from coordinates ;
+delete from sample_index ;
+delete from dataset ;
