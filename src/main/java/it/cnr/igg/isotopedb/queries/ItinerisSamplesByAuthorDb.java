7e11f2b1db857c76f12eb2839ac4bf6c20b7faf0
ericperrone <87177481+ericperrone@users.noreply.github.com>
2025-02-26 18:56:13 +0100

    itineris rest API

--- src/main/java/it/cnr/igg/isotopedb/queries/ItinerisSamplesByAuthorDb.java
+++ src/main/java/it/cnr/igg/isotopedb/queries/ItinerisSamplesByAuthorDb.java
@@ -1,5 +1,6 @@
 package it.cnr.igg.isotopedb.queries;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,28 +9,29 @@ import java.util.ArrayList;
 import it.cnr.igg.isotopedb.exceptions.DbException;
 import it.cnr.igg.isotopedb.exceptions.NotAuthorizedException;
 
-public class ItinerisMetadataByAuthorDb extends ItinerisCommon {
+public class ItinerisSamplesByAuthorDb extends ItinerisCommon {
 
-	public ItinerisMetadataByAuthorDb(String key) {
+	public ItinerisSamplesByAuthorDb(String key) {
 		super(key);
 	}
-	
-	public ArrayList<String> getMetadata(Long authorId) throws DbException, NotAuthorizedException {
+
+	public ArrayList<Integer> getSamples(Long authorId) throws DbException, NotAuthorizedException {
 		Connection con = null;
 		PreparedStatement ps = null;
-		String query = "select d.metadata from dataset d "
-				+ "where d.metadata is not null "
-				+ "and d.id in (select da.dataset_id from dataset_authors da where da.author_id = ?)";
-		ArrayList<String> metadata = new ArrayList<String>();
+		String query = "select si.sample_id from sample_index si "
+				+ "where si.dataset_id in ("
+				+ "select d.id from dataset d where d.id in (select da.dataset_id from dataset_authors da where da.author_id = ?))";
+				
+		ArrayList<Integer> ids = new ArrayList<Integer>();
 		try {
 			con = checkItinerisKey();
 			ps = con.prepareStatement(query);
 			ps.setInt(1, authorId.intValue());
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				metadata.add(rs.getString("metadata"));
+				ids.add(rs.getInt("sample_id"));
 			}
-			return metadata;
+			return ids;
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			throw new DbException(ex);
@@ -38,6 +40,5 @@ public class ItinerisMetadataByAuthorDb extends ItinerisCommon {
 				cm.closeConnection();
 			}
 		}
-		
 	}
 }
